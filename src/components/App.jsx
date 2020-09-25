ac63d0e5a2c6df351c9799cc9b6e50650f7be602
Ryan Moore <kiwi.ryanmoore@gmail.com>
2020-09-26 11:49:25 +1200

    removing YT stuff

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,7 +8,6 @@ import MainForm from './form/MainForm';
 import Preliminary from './form/PreliminaryForm';
 import NotFound from './NotFound';
 import About from './About';
-import Protune from './Protune';
 
 const App = () => {
 
@@ -65,23 +64,14 @@ const App = () => {
   return (
     <div>
       <Router>
-        <Link to="/">
+        <Link to="/" className="home-link">
+          <nav>
           <div className="central-header">
             <h1 className="heading">dialed</h1>
             <h2 className="subheading">dial in your bike.</h2>
           </div>
+          </nav>
         </Link>
-        <nav>
-          <a href="https://yt-industries.com">
-            <div className="logo"></div>
-          </a>
-          <div className="links">
-            <Link to="/about">about</Link>
-            <Link to="/protune">protune</Link>
-            <a href="https://yt-industries.com">yt-industries.com</a>
-          </div>
-        </nav>
-
         <Switch>
           
           <Route path="/results" render={(props) => (<MainForm {...props}
@@ -98,7 +88,6 @@ const App = () => {
           />)} />
 
           <Route path="/about" component={About} />
-          <Route path="/protune" component={Protune} />
           <Route component={NotFound} />
 
         </Switch>
